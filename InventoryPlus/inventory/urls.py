87a3a38dbecbd37c0b19e3eb9e5d656d88875fc3
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2024-07-29 09:04:48 +0300

    Adding dashboard

--- InventoryPlus/inventory/urls.py
+++ InventoryPlus/inventory/urls.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Dashboard
+    path('', views.dashboard, name='dashboard'),
+   
     # Product URLs
     path('products/', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
